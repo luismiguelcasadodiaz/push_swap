893d42c817380fcc27cfc6eead25c1d04d18dc18
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2024-01-30 00:17:41 +0100

    movements statistics

--- tests/test100.sh
+++ tests/test100.sh
@@ -3,6 +3,7 @@ for (( i=1; i<=3; i++ ))
 do
 	ARG=$(seq 0 5000 | sort -R | head -100 |  tr '\n' ' ')
 	NUM=$(./push_swap $ARG | wc -l)
+	./push_swap $ARG
 	if [ $(uname -s) = Linux ]
 	then
 		OK=$(./push_swap $ARG | ./checker_linux $ARG)
